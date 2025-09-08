cb7f551d9c7b40fe71210c4719ff2e37b92e9d0a
Scott Key <mscottkey@gmail.com>
2025-09-08 19:22:54 +0000

    https://genkit.dev/docs/get-started/

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,6 +1,6 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import {firebase} from '@genkit-ai/firebase/plugin';
+import {firebase} from '@genkit-ai/firebase';
 
 export const ai = genkit({
   plugins: [firebase(), googleAI()],
