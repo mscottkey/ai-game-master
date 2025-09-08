1bdc812986e2be78d3416f8a0f7f8f64db9167f4
Scott Key <mscottkey@gmail.com>
2025-09-08 19:24:09 +0000

    Also check this: https://firebase.google.com/docs/ai-logic?authuser=0&_g

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,8 +1,7 @@
 import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/googleai';
 import {firebase} from '@genkit-ai/firebase';
 
 export const ai = genkit({
-  plugins: [firebase(), googleAI()],
+  plugins: [firebase()],
   model: 'googleai/gemini-2.5-flash',
 });
